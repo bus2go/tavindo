c51137316c1ea2d26b24d3bc0f3cf2afd42e8cfe
Leonardo Alkmim <lalkmim@gmail.com>
2017-01-27 16:36:15 +0000

    setup-dev fix

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 7
+
 git submodule update --init --recursive
 
 cd tavindo-load-data && npm i && cd ..
